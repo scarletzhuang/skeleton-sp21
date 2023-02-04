48e42cf6259c140ebc9ea474d3700e8b693827ae
scarletzhuang <scarletzhuang@gmail.com>
2023-02-04 23:10:49 +0800

    project 1: capitalize constan.

--- proj1/gh2/GuitarHero.java
+++ proj1/gh2/GuitarHero.java
@@ -6,18 +6,18 @@ import edu.princeton.cs.algs4.StdDraw;
 
 public class GuitarHero {
 
-    public static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    public static final String KEYBOARD = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
 
     public static void main(String[] args) {
         ArrayDeque<GuitarString> guitarStrings = new ArrayDeque<>();
-        for (int i = 0; i < keyboard.length(); i++) {
+        for (int i = 0; i < KEYBOARD.length(); i++) {
             guitarStrings.addLast(new GuitarString(440.0 * Math.pow(2, (i - 24) / 12.0)));
         }
 
         while (true) {
             if (StdDraw.hasNextKeyTyped()) {
                 char key = StdDraw.nextKeyTyped();
-                int index = keyboard.indexOf(key);
+                int index = KEYBOARD.indexOf(key);
                 if (index == -1) {
                     continue;
                 } else {
